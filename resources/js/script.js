bf0ca7590537871d5ee47adc3d47875ecc4614b6
adamhayek2002 <YOUR_EMAIL>
2023-07-15 11:01:35 +0300

    frontend <> adding picture and getting v1 of hero section

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 let text = document.getElementById('text');
 let treeLeft = document.getElementById('tree-left');
 let treeRight = document.getElementById('tree-right');
-let gateLeft = document.getElementById('gate-left');
+let gateLeft = document.getElementById('santa');
 let gateRight = document.getElementById('gate-right');
 
 window.addEventListener('scroll', () =>{
