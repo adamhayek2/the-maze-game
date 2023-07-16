e5db6b158fe832b64f5293f95ec12f5954454a94
moeassi96 <117896769+moeassi96@users.noreply.github.com>
2023-07-16 11:54:35 +0300

    <phaser>added  MenuScene functionality

--- js/scenes/MenuScene.js
+++ js/scenes/MenuScene.js
@@ -14,46 +14,29 @@
     }
     create(){
 
-        this.add.image(200,200,'level1').setOrigin(0,0).setDisplaySize(200,150)
-        this.add.image(450,200,'level2').setOrigin(0,0).setDisplaySize(200,150)
-        this.add.image(700,200,'level3').setOrigin(0,0).setDisplaySize(200,150)
-        this.add.text(300, 380, 'Easy', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
-        this.add.text(550, 380, 'Medium', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
-        this.add.text(800, 380, 'Hard', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
+        this.level1 = this.add.image(120,250,'level1').setOrigin(0,0).setDisplaySize(250,200).setInteractive();
+        this.level2 = this.add.image(420,250,'level2').setOrigin(0,0).setDisplaySize(250,200).setInteractive();
+        this.level3 = this.add.image(720,250,'level3').setOrigin(0,0).setDisplaySize(250,200).setInteractive();
+        this.add.text(250, 480, 'Easy', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
+        this.add.text(550, 480, 'Medium', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
+        this.add.text(850, 480, 'Hard', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
         this.add.text(550, 80, 'MAZE RAIDER', { fontSize: '64px', fill: '#ffffff' }).setOrigin(0.5);
-        this.add.text(550, 170, 'Choose level', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
-        this.add.text(540, 500, 'Press Enter to Start', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
+        this.add.text(550, 200, 'Choose level', { fontSize: '24px', fill: '#ffffff' }).setOrigin(0.5);
+        
 
 
-        this.choice = "1";
+        this.level1.on("pointerdown", function() {
+            this.scene.start(CST.SCENES.MAZE1);
+        }, this);
 
-        this.input.on("pointerdown",function(pointer, level){
+        this.level2.on("pointerdown", function() {
+            this.scene.start(CST.SCENES.MAZE2);
+        }, this);
 
-            if (level.texture.key === 'level1') {
-                
-                console.log('Level 1 clicked');
-            } else if (level.texture.key === 'level2') {
-                
-                console.log('Level 2 clicked');
-            } else if (level.texture.key === 'level3') {
-                
-                console.log('Level 3 clicked');
-            }
-
-        },this);
-
-
+        this.level3.on("pointerdown", function() {
+            this.scene.start(CST.SCENES.MAZE3);
+        }, this);
 
         };
-
-    update(){
-
-        
-        let pressEnter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
-
-        if(pressEnter.isDown){
-            this.scene.start(CST.SCENES.MAZE1);
-        }
-    }
     
 }
